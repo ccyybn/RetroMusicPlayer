7b7e9f7997b624aa61a602d8c0d2cade9a5ba95f
cc <ccyybn@gmail.com>
2024-06-14 03:31:34 +0800

    Add forward and backward buttons

--- app/src/main/java/code/name/monkey/retromusic/App.kt
+++ app/src/main/java/code/name/monkey/retromusic/App.kt
@@ -78,7 +78,8 @@ class App : Application() {
         }
 
         fun isProVersion(): Boolean {
-            return BuildConfig.DEBUG || instance?.billingManager!!.isProVersion
+//            return BuildConfig.DEBUG || instance?.billingManager!!.isProVersion
+            return true
         }
     }
 }
--- app/src/main/java/code/name/monkey/retromusic/util/PreferenceUtil.kt
+++ app/src/main/java/code/name/monkey/retromusic/util/PreferenceUtil.kt
@@ -688,6 +688,8 @@ object PreferenceUtil {
             false
         )
 
+    val seekSeconds get() = sharedPreferences.getInt(SEEK_SECONDS, 5)
+
     var audioFadeDuration
         get() = sharedPreferences
             .getInt(AUDIO_FADE_DURATION, 0)
